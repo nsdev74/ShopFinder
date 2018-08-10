4db6865c23b849d2a40c4f76a6557c6b418b8993
nsdev74 <nakach.salim@gmail.com>
2018-08-10 17:57:32 +0100

    Add header and modified base config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShopsComponent } from './shops/shops.component';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ShopsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
+import { AppRoutingModule } from '../app-routing.module';
+
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFontAwesomeModule
   ],
-  declarations: [HeaderComponent, HomeComponent]
+  declarations: [HeaderComponent, HomeComponent],
+  exports: [
+    HeaderComponent,
+    AppRoutingModule
+  ]
 })
 export class CoreModule { }
