80616a78a8b6efcc2fe7b4d0914acc92bcb75fb4
nsdev74 <nakach.salim@gmail.com>
2018-08-10 18:59:23 +0100

    Add dummy shop service

--- src/app/shops/shops.model.ts
+++ src/app/shops/shops.model.ts
@@ -1,6 +1,22 @@
-export interface Shop {
+
+/* export interface Shop {
   shopName: string;
   shopDistance: number;
   shopImagePath: string;
   shopId: number;
+} */
+
+// We need to test front end elements using a class with a constructor, placeholder block
+export class Shop {
+  public shopName: string;
+  public shopDistance: number;
+  public shopImagePath: string;
+  public shopId: number;
+
+  constructor(name: string, dist: number, imgPath: string, id: number) {
+    this.shopName = name;
+    this.shopDistance = dist;
+    this.shopImagePath = imgPath;
+    this.shopId = id;
+  }
 }
---
+++ src/app/shops/shops.service.ts
@@ -0,0 +1,60 @@
+import { Shop } from './shops.model';
+
+export class ShopsService {
+  // Dummy data for testing purposes
+  private shops: Shop[] = [
+    new Shop(
+      'Shop1',
+      50,
+      'http://placehold.it/150x150',
+      1
+    ),
+    new Shop(
+      'Shop2',
+      40,
+      'http://placehold.it/150x150',
+      2
+    ),
+    new Shop(
+      'Shop3',
+      30,
+      'http://placehold.it/150x150',
+      3
+    ),
+    new Shop(
+      'Shop4',
+      20,
+      'http://placehold.it/150x150',
+      4
+    ),
+    new Shop(
+      'Shop5',
+      19,
+      'http://placehold.it/150x150',
+      5
+    ),
+    new Shop(
+      'Shop6',
+      18,
+      'http://placehold.it/150x150',
+      6
+    ),
+    new Shop(
+      'Shop7',
+      17,
+      'http://placehold.it/150x150',
+      7
+    ),
+    new Shop(
+      'Shop8',
+      16,
+      'http://placehold.it/150x150',
+      8
+    )
+  ];
+
+  getShops() {
+    // Dummy function
+    return this.shops.slice();
+  }
+}
