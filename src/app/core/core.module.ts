0aa25be3eddef89e35a295ae76adebd74817fb41
nsdev74 <nakach.salim@gmail.com>
2018-08-10 18:37:42 +0100

    Add navbar navigation for auth pages

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-
 import { AppRoutingModule } from '../app-routing.module';
-
 
 @NgModule({
   imports: [
     CommonModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AppRoutingModule
   ],
   declarations: [HeaderComponent, HomeComponent],
   exports: [
