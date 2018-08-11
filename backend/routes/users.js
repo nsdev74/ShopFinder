9ce93b664b78e23dc53f8f370d3ea48df7e4e771
nsdev74 <nakach.salim@gmail.com>
2018-08-11 17:47:38 +0100

    Add user sign in POST function w/ Json Web Token

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const {User} = require('../models/user');
+const secret = 'secret_text_that_is_obviously_not_complicated_or_secure_enough';
 
 const router = express.Router();
 
@@ -25,6 +27,37 @@ router.post("/signup", (req,res,next) => {
             error: err
           });
         });
+    })
+})
+
+// User sign in POST
+router.post("/signin", (req,res,next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email }).then( user => {
+    // If user is not found, authentication fails
+    if (!user) {
+      return res.status(401);
+    } else {
+      fetchedUser = user;
+    }
+    // Return hashed password comparison for .then call
+    return bcrypt.compare(req.body.password, fetchedUser.password);
+  })
+    .then(result => {
+      if (!result) {
+        // If newly hashed password doesn't equal stored hashed password, authentication fails
+        return res.status(401);
+      } else {
+        let token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id}, secret, { expiresIn: '2h'});
+        res.statusCode(200).json({
+          token: token
+        })
+      }
+    })
+    .catch(err => {
+      // If other error, authentication fails
+      return res.status(401);
+      console.log(err);
     })
 })
 
