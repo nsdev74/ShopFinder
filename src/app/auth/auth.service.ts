3ab807801fcf0c3c4627c84262aa7b95e4fb44ee
nsdev74 <nakach.salim@gmail.com>
2018-08-11 15:23:35 +0100

    Add auth-data (user account) model

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthData } from './auth-data.model';
 
 @Injectable()
 
@@ -9,11 +10,15 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   signUp(email: string, password: string) {
-    const user = {email: email, password: password};
+    const user: AuthData = {email: email, password: password};
     this.http.post(
     'http://localhost:3000/api/users/signup', user
     ).subscribe ( res => {
+      if (res.message === 'Success!') {
       console.log(res);
+      // Redirecting to sign in page
+      this.router.navigate(['/signin']);
+      }
     });
   }
 }
