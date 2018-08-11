96a88fb1ac0b38a3768e6c747dc093b514c0c525
nsdev74 <nakach.salim@gmail.com>
2018-08-11 22:28:37 +0100

    Add authentication header to shop module operations

---
+++ src/app/auth/auth-interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '../../../node_modules/@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '../../../node_modules/@angular/common/http';
+
+import { AuthService } from './auth.service';
+
+
+@Injectable()
+
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const authToken = this.authService.getToken();
+    const authRequest = req.clone({
+      // Replacing default Authorization header with "Bearer {{token}}"
+      headers: req.headers.set('Authorization', 'Bearer ' + authToken)
+    });
+    return next.handle(authRequest);
+  }
+
+}
--- src/app/shops/shops.module.ts
+++ src/app/shops/shops.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { NearbyComponent } from './nearby/nearby.component';
 import { PreferredComponent } from './preferred/preferred.component';
 import { ShopsComponent } from './shops.component';
 import { ShopsRoutingModule } from './shops-routing.module';
 import { ShopsService } from './shops.service';
+import { AuthInterceptor } from '../auth/auth-interceptor';
+import { HTTP_INTERCEPTORS } from '../../../node_modules/@angular/common/http';
 
 @NgModule({
   imports: [
@@ -14,6 +17,6 @@ import { ShopsService } from './shops.service';
   ],
   declarations: [NearbyComponent, PreferredComponent, ShopsComponent],
   exports: [ShopsComponent],
-  providers: [ShopsService]
+  providers: [ShopsService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
 })
 export class ShopsModule { }
