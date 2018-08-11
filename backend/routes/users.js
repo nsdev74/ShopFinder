e294ed8f914f86a3d84dc922816f7392c3c4ea11
nsdev74 <nakach.salim@gmail.com>
2018-08-11 15:00:20 +0100

    Add user POST function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
+
 const {User} = require('../models/user');
 
 const router = express.Router();
 
-// Dummy call
-router.use( (req,res, next) => {
-  console.log('Hello world!');
-  res.send('Hello world from express!');
+// User sign up POST
+router.post("/signup", (req,res,next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      let user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(results => {
+          res.status(201).json({
+            message: 'Success!',
+            result: results
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    })
 })
 
 module.exports = router;
