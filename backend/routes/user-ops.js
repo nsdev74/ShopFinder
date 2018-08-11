19f5d5101304dd3636412d370d946910ec8d3445
nsdev74 <nakach.salim@gmail.com>
2018-08-11 22:15:07 +0100

    Add server authentication on dummy Shop GET function

--- backend/routes/user-ops.js
+++ backend/routes/user-ops.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {User} = require('../models/user');
 const {Shop} = require('../models/shop');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
 // Dummy GET shops function
-router.get('/shops', (req, res) => {
+router.get('/shops', checkAuth, (req, res) => {
   Shop.find().then( shops => {
     res.status(200).json({
       message: 'Success!',
