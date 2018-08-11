0b79d588a6cd6395b249c12d82f23e100ffe1b5d
nsdev74 <nakach.salim@gmail.com>
2018-08-11 19:16:35 +0100

    Add backend shop model

---
+++ backend/models/shop.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+// Shop Schema
+var shopSchema = new mongoose.Schema({
+    picture: {
+         type: String,
+         trim: true
+        },
+    name: {
+        type: String,
+        trim: true
+    },
+    email: { type: String},
+    city: { type: String },
+    location : {
+         type: {
+            type: String
+         },
+         coordinates: [Number,Number]
+        }
+})
+
+// Shop Model
+var Shop = mongoose.model('Shop', shopSchema)
+
+
+module.exports = {Shop}
